a4273b6bcd2516c4faeafb81050c859bfd2a8bdf
kvburke <kvburke@ncsu.edu>
2015-02-06 14:32:16 -0500

    Update main.cpp

--- AnalyticsWithDatabase/main.cpp
+++ AnalyticsWithDatabase/main.cpp
@@ -13,26 +13,27 @@ int main() /*Main function*/
 
 
 
-    Engine engine;
+    Engine *engine = new Engine();
 
-    engine.createtable(); /*creates table in database */
-    engine.displaymoneyvstime(); /*displaymoneyvstime - displays table to stdout and sumwebservice.xml*/
+    engine->createtable(); /*creates table in database */
+    engine->displaymoneyvstime(); /*displaymoneyvstime - displays table to stdout and sumwebservice.xml*/
     cout << "Enter money amount (1-10000)" << endl;
     cin >> inputone;
     cout << "Enter time of day (0-24)" << endl;
     cin >> inputtwo;
 
-    engine.inputmoneyvstime(inputone,inputtwo); /*inputmoneyvstime - inputs record into table */
-    engine.updatemoneyvstimexml(); /*Updates XML file with newly acquired data */
+    engine->inputmoneyvstime(inputone,inputtwo); /*inputmoneyvstime - inputs record into table */
+    engine->updatemoneyvstimexml(); /*Updates XML file with newly acquired data */
     cout << "Enter initial time of day for money average:" << endl; /*Get user input to acquire avg over time*/
     cin >> initialrange;
     cout << "Enter final time for money average: " << endl;
     cin >> finalrange;
 
 
-    engine.meanmoney(initialrange,finalrange);  /*meanmoney - calculates average over a specific day range*/
-    engine.sorter(); /*sorts all money from least to greatest*/
-
+    engine->meanmoney(initialrange,finalrange);  /*meanmoney - calculates average over a specific day range*/
+    engine->sorter(); /*sorts all money from least to greatest*/
+    delete engine;
+    return 0;
 }
                             /*example: engine.inputmoneyvstime(5,10) Inputs record Day-5 Money-10*/
                             /*example: engine.meanmoney(2,4) Calculates average over days 2 to 4*/
